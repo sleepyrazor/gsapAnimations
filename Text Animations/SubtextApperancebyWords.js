173a49092fb188ac2c7ada60eb8ec37d3e2026ca
Paula Dolado Aynié <78765611+PaulaDolado@users.noreply.github.com>
2025-08-14 19:28:01 +0200

    Update SubtextApperancebyWords.js

--- Text Animations/SubtextApperancebyWords.js
+++ Text Animations/SubtextApperancebyWords.js
@@ -1,14 +1,19 @@
+//Remember to implement in your html file the correct Gsap CDN like the following ones.
+//<script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"></script>
+//<script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/SplitText.min.js"></script>
+
+// First declare the gsap plugin
 gsap.registerPlugin(SplitText);
 
-// Cogemos los elementos
+// Declare the elements
 const subtext = document.querySelector(".subtext");
 const subtext2 = document.querySelector(".subtext2");
 
-// Dividir texto en letras y palabras
+// Split text into words.
 const splitSubText=new SplitText(subtext, {type: "words"})
 const splitSubText2=new SplitText(subtext2, {type: "words"})
   
-//Animación subtítulo en palabras
+// The title appears from the bottom to the top with a blurring effect.
 tl.from(splitSubText.words,{
     y:60,
     opacity:0,
@@ -25,12 +30,3 @@ tl.from(splitSubText2.words,{
     duration:0.7,
     ease: "power2.out"
 }, "-=0.5");
-
-tl.to(".btn", {
-    visibility: "visible",
-    opacity: 1,
-    y: 0,
-    duration: 0.8,
-    ease: "back.out(1.2)", 
-    delay: 0.1 
-}); 
